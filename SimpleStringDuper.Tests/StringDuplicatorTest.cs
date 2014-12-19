769f05a8fb358cc2d08fd8c205c28c3325d36939
jx <jose.diaz@cdicorp.com>
2014-12-19 13:45:22 -0700

    Extension method shell added along with simple test case.

---
+++ SimpleStringDuper.Tests/StringDuplicatorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleStringDuper;
+
+namespace SimpleStringDuper.Tests
+{
+    [TestClass]
+    public class StringExtensionTest
+    {
+        [TestMethod]
+        public void Test_Duplicate()
+        {
+            string str = "this is only a test.";
+            string expectedString = "tthhiiss  iiss  oonnllyy  aa  tteesstt..";
+            Assert.AreEqual(expectedString, str.Duplicate());
+        }
+    }
+}
---
+++ SimpleStringDuper/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStringDuper
+{
+    public static class StringExtension
+    {
+        public static string Duplicate(this String str)
+        {
+            return string.Empty;
+        }
+    }
+}
