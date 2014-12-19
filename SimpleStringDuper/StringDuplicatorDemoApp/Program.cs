da6f4a930ff54d6385855b6a37470112feeb1229
jx <jose.diaz@cdicorp.com>
2014-12-19 15:06:33 -0700

    Add a simple console app to test library

---
+++ SimpleStringDuper/StringDuplicatorDemoApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleStringDuper;
+
+namespace StringDuplicatorDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StringDuplicator Demo Application.");
+            Console.Write("Enter the test string:");
+            string input = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(input.Duplicate());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
